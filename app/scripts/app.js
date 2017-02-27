b0021d239fd37107874581bf852fbbaae0950852
Igor Gorlov <gorlov35@gmail.com>
2017-02-28 02:01:52 +0300

    add index page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,17 @@
 import svg4everybody from 'svg4everybody';
-import $ from 'jquery';
+// import $ from 'jquery';
 
-$(() => {
+$(($) => {
 	svg4everybody();
+
+	const $navTrigger = $('.nav__trigger');
+	const $navList = $('.nav__list');
+
+	$navTrigger.on('click', function (e) {
+		e.preventDefault();
+		$(this).toggleClass('nav__trigger_active');
+		$navList.toggleClass('nav__list_open');
+	});
+
+
 });
